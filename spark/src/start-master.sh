66125c0da533e3b1e646862f12d9937f512be94e
Hien Vien <hienmvien@gmail.com>
2016-01-21 23:37:12 -0800

    Added separate docker compose files for both sparkmaster and sparkworker.

--- spark/src/start-master.sh
+++ spark/src/start-master.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -z "$SPARK_MASTER_HOST" ]; then echo "SPARK_MASTER_HOST is unset. Quitting" && exit 1; fi
+if [ -z "${SPARK_MASTER_HOST}" ]; then echo "SPARK_MASTER_HOST is unset. Quitting" && exit 1; fi
 
-export SPARK_CLASSPATH="$SPARK_HOME/sbin/../conf/:$SPARK_HOME/lib/*"
+export SPARK_CLASSPATH="${SPARK_HOME}/sbin/../conf/:${SPARK_HOME}/lib/*"
 
-$JAVA_HOME/bin/java -cp $SPARK_CLASSPATH \
+${JAVA_HOME}/bin/java -cp ${SPARK_CLASSPATH} \
 	-Dspark.driver.port=7001 -Dspark.fileserver.port=7002 \
 	-Dspark.broadcast.port=7003 -Dspark.replClassServer.port=7004 \
 	-Dspark.blockManager.port=7005 -Dspark.executor.port=7006 -Dspark.ui.port=4040 \
 	-Dspark.broadcast.factory=org.apache.spark.broadcast.HttpBroadcastFactory -Xms1g \
-	-Xmx1g org.apache.spark.deploy.master.Master --host $SPARK_MASTER_HOST --port 7077 --webui-port 8080 \
+	-Xmx1g org.apache.spark.deploy.master.Master --host ${SPARK_MASTER_HOST} --port 7077 --webui-port 8080 \
 	--properties-file /spark-defaults.conf
