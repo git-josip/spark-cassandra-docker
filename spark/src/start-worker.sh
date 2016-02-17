7b514b98b31fe5d61a7c0cb858b45138b8fb364a
Raimundo Onetto <raimundo@foxtrot.io>
2016-02-16 16:10:47 -0800

    Spark master url fixes

--- spark/src/start-worker.sh
+++ spark/src/start-worker.sh
@@ -8,6 +8,6 @@ ${JAVA_HOME}/bin/java -cp ${SPARK_CLASSPATH} \
 	-Dspark.blockManager.port=7005 -Dspark.executor.port=7006 -Dspark.ui.port=4040 \
 	-Dspark.broadcast.factory=org.apache.spark.broadcast.HttpBroadcastFactory \
 	-Dspark.worker.cleanup.appDataTtl=86400 -Dspark.worker.cleanup.interval=1800 \
-	-Xms1g -Xmx1g org.apache.spark.deploy.worker.Worker spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} \
+	-Xms1g -Xmx1g org.apache.spark.deploy.worker.Worker \
 	--webui-port 8081 --properties-file /spark-defaults.conf --work-dir ${SPARK_WORK_DIR} \
-	-i ${SPARK_LOCAL_IP} --port 8888 
+	-i ${SPARK_LOCAL_IP} --port 8888 spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} 
